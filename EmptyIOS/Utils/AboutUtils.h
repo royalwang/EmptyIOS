ef1d62029b8f071e5e1f7a65757d50aa9581f643
Zoro L <757111858@qq.com>
2014-03-27 21:37:42 -0700

    add a group of utile

---
+++ EmptyIOS/Utils/AboutUtils.h
@@ -0,0 +1,17 @@
+//
+//  AboutUtils.h
+//  EmptyIOS
+//
+//  这个目录放一些通用的工具类，如字符串处理，图片处理等
+//  put common utils in this group
+//
+//  Created by LGQ on 14-3-27.
+//  Copyright (c) 2014年 LGQ. All rights reserved.
+//s
+
+#ifndef EmptyIOS_AboutUtils_h
+#define EmptyIOS_AboutUtils_h
+
+
+
+#endif
